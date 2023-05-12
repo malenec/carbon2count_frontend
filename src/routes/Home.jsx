5236e36f2a1084c4e1638fa9d9cffd65f2c6baaa
Israa1809 <din-israa@hotmail.com>
2023-05-12 13:08:19 +0200

    refactor deletes old code with quote

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,14 +7,13 @@ const Home = ({ user }) => {
     <div>
       <h3>Home</h3>
       <h1> Welcome to CARBON2COUNT </h1>
-      {/* <Quote user={user} /> */}
 
       {user.username === "" ? (<h4>Log in to see and create grocery lists </h4>) :
         (<>
-      <NavLink to="/grocerylist">
-        <button>New List</button>
-      </NavLink>
-        
+          <NavLink to="/grocerylist">
+            <button>New List</button>
+          </NavLink>
+
         </>)}
 
     </div>
